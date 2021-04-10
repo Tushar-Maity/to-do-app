e7a5a2f5624324db5a44edf86a8bf6fee7ecf32e
Tushar989-Maity <68024552+Tushar989-Maity@users.noreply.github.com>
2021-04-10 18:58:52 +0530

    Update Todo.js

--- src/Todo.js
+++ src/Todo.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { List, ListItem, ListItemAvatar, ListItemText } from '@material-ui/core';
+import { Button, List, ListItem, ListItemAvatar, ListItemText } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import db from './firebase';
 import './Todo.css';
 
 function Todo(props) {
+
+
     return (
         <List className="todo__list">
             <ListItem>
             <ListItemAvatar>
             </ListItemAvatar>
-                <ListItemText primary={props.text} secondary="Dummy deadline🎅"/>
+                <ListItemText primary={props.todo.todo} secondary="Dummy deadline🎅"/>
             </ListItem>
+            <Button onClick={event => 
+            db.collection('todos').doc(props.todo.id).delete()}>❌DELETE ME</Button>
         </List>
     )
 }
+
 export default Todo
